212afbaca9b8d03b5832a6a7400216d30a467efd
田爪大智 <>
2022-11-02 15:35:32 +0900

    #17

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,11 +1,7 @@
 class MypageController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    @mypages = current_user.articles.order(updated_at: :desc)
-  end
 
   def show
-    @mypage = current_user.articles.find(params[:id])
+    @articles = current_user.articles.order(updated_at: :desc)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   root to: "articles#index"
-  resources :mypage
+  get "/mypage", to: "mypage#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
