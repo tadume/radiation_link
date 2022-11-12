3f500ac473b5108c7c25fd86bd37b759fbaeab7e
田爪大智 <>
2022-11-13 08:48:44 +0900

    #31

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   resources :articles
   root to: "articles#index"
   get "/mypage", to: "mypage#show"
   resources :comments, only: [:create]
+  resources :users, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
