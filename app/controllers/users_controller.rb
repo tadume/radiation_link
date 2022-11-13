9772e71c7dcabb56e022e1bd602eaa845e75eb32
田爪大智 <>
2022-11-13 11:09:01 +0900

    #35

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(updated_at: :desc).page(params[:page]).per(1)
+    # @users = User.order(updated_at: :desc).page(params[:page]).per(1)
+    users = User.where("name LIKE ?", "%#{ params[:name] }%")
+    @users = users.order(updated_at: :desc).page(params[:page]).per(1)
   end
 end
