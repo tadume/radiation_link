0af9bd896c5866ba9c2b406aa16a6cfaaec96a4c
田爪大智 <>
2024-01-22 05:45:00 +0900

    user-articlesリレーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,6 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #  index_users_on_uid_and_provider      (uid,provider) UNIQUE
 #
-# class User < ActiveRecord::Base
 class User < ApplicationRecord
   extend Devise::Models
   # Include default devise modules. Others available are:
@@ -38,4 +37,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :articles, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_21_105644) do
+ActiveRecord::Schema.define(version: 2024_01_21_202828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -39,4 +48,5 @@ ActiveRecord::Schema.define(version: 2024_01_21_105644) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
