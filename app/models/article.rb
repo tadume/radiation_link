d0fb3e1e4f9ba66ef138adf9f1e1389f20f01bb0
田爪大智 <>
2024-01-22 07:15:11 +0900

    各モデルにvalidationを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,4 +21,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 120 }
 end
